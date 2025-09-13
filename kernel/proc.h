d372b5db94aada9b22d8003255122af0bee5dd15
flowinglight-yu <flowinglight-yu@outlook.com>
2025-09-13 17:37:12 +0800

    完成lab3的第二个实验

--- kernel/proc.h
+++ kernel/proc.h
@@ -103,4 +103,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  pagetable_t kernelpgtbl;     // 为进程添加自己的内核页表，避免使用原有的同一个内核页表
 };
